fc5e45cf4a15e09a6055ee3f7eef5cd0d288bf5d
serjio1905 <serjio.rv@gmail.com>
2018-10-27 20:14:58 +0200

    20181027

---
+++ startup/server.ts
@@ -0,0 +1,28 @@
+import * as express from 'express';
+import * as cors from 'cors';
+import * as helmet from 'helmet';
+import * as morgan from 'morgan';
+import * as compression from 'compression';
+
+class Server {
+    private _server;
+    
+    public get server() {
+        return this._server;
+    }
+
+    constructor() {
+        this._server = express();
+        this.init();
+    }
+
+    private init() {
+        this._server.use(helmet);
+        this._server.use(cors());
+        this._server.use(express.json());
+        this._server.use(compression);
+        this._server.use(morgan('tiny'));
+    }
+}
+
+export default new Server().server;
